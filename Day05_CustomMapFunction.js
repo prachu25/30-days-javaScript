866843286037097f5cc2cffa60ef8854cf50bd28
Prachi Gorde <gordeprachi64@gmail.com>
2025-12-16 12:01:03 +0530

    Add custom map function without Array.map

---
+++ Day05_CustomMapFunction.js
@@ -0,0 +1,25 @@
+// Day05_CustomMapFunction
+
+/**Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+Please solve it without the built-in Array.map method. */
+
+var map = function(arr, fn) {
+    let result = [];
+
+    for(let i=0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+
+    return result;
+    
+};
+
+// function to pass
+function plusOne(n) {
+    return n + 1;
+}
+
+// calling map
+let output = map([1, 2, 3], plusOne);
+console.log(output);   // [2, 3, 4]
