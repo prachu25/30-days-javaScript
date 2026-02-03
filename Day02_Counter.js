f01d7a38e1ea3f8a2087aefd26086afec09c2efd
Prachi Gorde <gordeprachi64@gmail.com>
2026-02-03 09:46:29 +0530

    Update Day02 Counter logic

--- Day02_Counter.js
+++ Day02_Counter.js
@@ -3,16 +3,13 @@ This counter function initially returns n and then returns 1 more than
 the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 */
 
-var createCounter = function(n) {
+let createCounter = function(n){
     return function(){
-       return n++;
-    
+        return n++;
     }
-};
+}
 
-let result = createCounter(4);
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
+let result = createCounter(9);
+console.log(result());   //9
+console.log(result());   //10
 
